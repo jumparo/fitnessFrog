f841b98cc56666f11720bb68df8cdff7e21c7e64
Borislav Yordanov <bobi-npmg@hotmail.com>
2016-11-26 14:40:45 +0200

    Updated the Add Entry page to redirect to the Entry list page

--- aspnet-fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ aspnet-fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -55,7 +55,7 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
-
+                return RedirectToAction("Index");
                 // TODO Display the Entries list page
             }
 
